6e8a4cdfe5d521e61eeac8757bb0d9a373f378ff
roophil <6032018+robophil@users.noreply.github.com>
2023-12-02 19:12:38 +0100

    fix: solve day 1 part 1

--- src/questions/day1.rs
+++ src/questions/day1.rs
@@ -3,52 +3,40 @@ use crate::solutions::{Solution, SolutionResult};
 #[derive(Default)]
 pub struct Question;
 
-struct Count {
-    last_value: i32,
-    count: i32,
-    first_run: bool,
-}
 impl Question {
-    fn solve(&self, input: &Vec<String>) -> SolutionResult {
-        let result: Count = input.iter().fold(
-            Count {
-                last_value: 0,
-                count: 0,
-                first_run: true,
-            },
-            |acc: Count, f: &String| {
-                let num: i32 = f.parse::<i32>().unwrap();
-                if acc.first_run {
-                    Count {
-                        last_value: num,
-                        count: acc.count,
-                        first_run: false,
-                    }
-                } else if num > acc.last_value {
-                    Count {
-                        last_value: num,
-                        count: acc.count + 1,
-                        first_run: false,
-                    }
-                } else {
-                    Count {
-                        last_value: num,
-                        count: acc.count,
-                        first_run: false,
+    fn solve_1(&self, input: &Vec<String>) -> SolutionResult {
+        let sum = input.iter().fold(0, |acc, line| {
+            let mut first = 0;
+            let mut second = 0;
+            let mut first_number_found = false;
+
+            for c in line.chars() {
+                if c.is_numeric() {
+                    if !first_number_found {
+                        first_number_found = true;
+                        first = c.to_digit(10).unwrap() * 10;
+                        second = c.to_digit(10).unwrap();
+                    } else {
+                        second = c.to_digit(10).unwrap();
                     }
                 }
-            },
-        );
-        Ok(result.count.to_string())
+            }
+            acc + first + second
+        });
+        Ok(sum.to_string())
+    }
+
+    fn solve_2(&self, input: &Vec<String>) -> SolutionResult {
+        Ok("".to_string())
     }
 }
 
 impl Solution for Question {
     fn solve_input_1(&self, input: &Vec<String>) -> SolutionResult {
-        self.solve(input)
+        self.solve_1(input)
     }
 
     fn solve_input_2(&self, input: &Vec<String>) -> SolutionResult {
-        self.solve(input)
+        self.solve_2(input)
     }
 }
