4f3cfd231b82cc0aeeef664f739b0fa4b324abf8
roophil <6032018+robophil@users.noreply.github.com>
2023-12-02 12:20:02 +0100

    fix: specify type explicitly

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Welcome to AOC 2023");
     let solutions: Vec<Box<dyn Solution>> =
         vec![Box::new(day1::Question), Box::new(day2::Question)];
-    let mut day = String::new();
-    let mut part = String::new();
+    let mut day: String = String::new();
+    let mut part: String = String::new();
 
     println!("Enter the day you want to solve for. Eg for day, enter 1 - 25 :");
     io::stdin().read_line(&mut day)?;
@@ -29,17 +29,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Enter either 1 or 2, to solve the first or second problem :");
     io::stdin().read_line(&mut part)?;
 
-    let start_time = SystemTime::now();
+    let start_time: SystemTime = SystemTime::now();
 
-    let day = day.trim();
-    let part = part.trim();
+    let day: &str = day.trim();
+    let part: &str = part.trim();
 
     println!("AOC:2023 Solving Day {}, part {} ....", day, part);
 
-    let path_to_file = get_input_file_path(day, part);
-    let input = file_to_string_vec(&path_to_file)?;
+    let path_to_file: String = get_input_file_path(day, part);
+    let input: Vec<String> = file_to_string_vec(&path_to_file)?;
 
-    let solution = &solutions[day.parse::<usize>()? - 1];
+    let solution: &Box<dyn Solution> = &solutions[day.parse::<usize>()? - 1];
 
     if part == "1" {
         println!("Solution: {}", solution.solve_input_1(&input)?);
