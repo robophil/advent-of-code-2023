dcb58f8ab0db0df7101e03326a6a80ee215791d7
roophil <6032018+robophil@users.noreply.github.com>
2023-12-03 23:19:53 +0100

    feat: day 2 part 2

--- src/questions/day2.rs
+++ src/questions/day2.rs
@@ -23,6 +23,25 @@ impl Question {
         true
     }
 
+    fn get_minimum_cube_power(&self, color_parts: &str) -> i32 {
+        // 3 blue, 4 red, 1 red, 2 green, 6 blue, 2 green
+        let mut color_map: HashMap<&str, i32> = HashMap::from([
+            ("red", 1),
+            ("green", 1),
+            ("blue", 1)
+        ]);
+        for color_part in color_parts.split(",") {
+            let mut color_part = color_part.trim().split(" ");
+            let color_count = color_part.next().unwrap().trim().parse::<i32>().unwrap();
+            let color_name = color_part.next().unwrap().trim();
+            let previous_count = color_map.get(color_name).unwrap();
+            if &color_count > previous_count  {
+                color_map.insert(color_name, color_count);
+            }
+        }
+        color_map.iter().fold(1, |acc, (_, count)| acc * count)
+    }
+
     fn solve_1(&self, input: &Vec<String>) -> SolutionResult {
         let game_sum = input.iter().fold(0, |acc, line| {
             let mut parts = line.split(":");
@@ -44,6 +63,18 @@ impl Question {
         });
         Ok(game_sum.to_string())
     }
+
+    fn solve_2(&self, input: &Vec<String>) -> SolutionResult {
+        let power_sum = input.iter().fold(0, |acc, line| {
+            let mut parts = line.split(":");
+            parts.next();
+            let color_parts = parts.next().unwrap();
+
+            let color_parts = color_parts.replace(";", ",");
+            acc + self.get_minimum_cube_power(&color_parts)
+        });
+        Ok(power_sum.to_string())
+    }
 }
 
 impl Solution for Question {
@@ -52,6 +83,6 @@ impl Solution for Question {
     }
 
     fn solve_input_2(&self, input: &Vec<String>) -> SolutionResult {
-        self.solve_1(input)
+        self.solve_2(input)
     }
 }
